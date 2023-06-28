cdb54eaa770013bf981e6d814c98482a45330f0d
wegulohillary <wegulohillary@gmail.com>
2023-06-27 17:48:47 -0700

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,32 @@
+#!/usr/bin/python3
+""" pascal triangle
+"""
+
+
 def pascal_triangle(n):
+    """ returns pascal triangle
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # First row of the triangle
+    pasTran = []
 
-    for i in range(1, n):
-        row = [1]  # First element of each row is always 1
+    for i in range(n):
+        # first element
+        my_List = [1]
+        if i == 0:
+            pasTran.append(my_List)
+            continue
 
-        # Calculate the elements in the current row
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        k = i - 1
+        for j in range(len(pasTran[k])):
+            if j + 1 == len(pasTran[k]):
+                # last element
+                my_List.append(1)
+                break
+            # Add two previous values to get current next value
+            nextVal = pasTran[k][j] + pasTran[k][j + 1]
+            my_List.append(nextVal)
+        pasTran.append(my_List)
 
-        row.append(1)  # Last element of each row is always 1
-        triangle.append(row)
-
-    return triangle
+    return pasTran
